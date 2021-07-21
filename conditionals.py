00014f88cff70b894cb7fa9c84f6b7fdea22fdeb
Fomonyuytar Joseph <josephneymar33@gmail.com>
2021-07-20 18:07:02 -0700

    init

---
+++ conditionals.py
@@ -0,0 +1,7 @@
+age=19
+if age<4:
+  print('admission is free')
+elif age<18:
+    print('your admission fee is 5$')
+else:
+    print('your admission costs 10$')
---
+++ toppings.py
@@ -0,0 +1,8 @@
+requested_toppings=['tomato','egg']
+if 'egg' in requested_toppings:
+    print('adding your egg')
+if 'cabbage' in requested_toppings:
+    print('adding your cabbage')
+if 'tomato' in requested_toppings:
+    print('adding your tomato')
+print('done  making you pizza')
