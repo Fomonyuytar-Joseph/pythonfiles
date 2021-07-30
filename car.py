22f4e30500225978d1a220f55613df3083f35dc5
Fomonyuytar Joseph <josephneymar33@gmail.com>
2021-07-30 12:35:55 -0700

    init

--- car.py
+++ car.py
@@ -10,7 +10,15 @@ class Car():
     def read_odometer(self):
         """Print a statement showing the car's mileage."""
         print("This car has " + str(self.odometer_reading) + " miles on it.")
+    def update_odometer(self, mileage):
+       if mileage >= self.odometer_reading:
+           self.odometer_reading = mileage
+       else:
+           print("You can't roll back an odometer!")
+    def increment_odometer(self, miles):
+      self.odometer_reading += miles
 
+    
 my_new_car=Car('audi','a4',2016)
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
