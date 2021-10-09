2d723faca10a84e96eb0a23421cb8436dc162380
Fomonyuytar Joseph <josephneymar33@gmail.com>
2021-10-09 13:39:53 -0700

    init

---
+++ listmethods.py
@@ -0,0 +1,30 @@
+numbers=list(range(1,5))
+print(numbers)
+
+even_numbers=list(range(2,11,2))
+print(even_numbers)
+
+squares=[]
+for value in range(1,13):
+    square=value**2
+    squares.append(square)
+print(squares)
+
+items=[1,2,3,4,5]
+val=[]
+for item in items:
+    item=item**2
+    val.append(item)
+print(val)
+    
+
+
+
+
+
+
+
+# magicians=["mark",'james','joseph','max']
+
+# for magician in magicians:
+#  print(magician.title()+", it was a great trick")
