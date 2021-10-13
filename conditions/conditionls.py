58cd7940b22e657d8153926c98036a3c8609d119
Fomonyuytar Joseph <josephneymar33@gmail.com>
2021-10-12 21:58:58 -0700

    init

---
+++ conditions/conditionls.py
@@ -0,0 +1,6 @@
+requested_toppings=[]
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print("adding "+ requested_toppings +".")
+else:
+    print("are you not sure you a plain pizza")
