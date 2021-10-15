8d82a6878c992f9a7798e69b9e4a71695f6c40f6
Fomonyuytar Joseph <josephneymar33@gmail.com>
2021-10-14 19:44:19 -0700

    init

---
+++ functions/greetings.py
@@ -0,0 +1,25 @@
+import user_profile
+
+my_profile=user_profile.build_profile('fomonyuytar','joseph',location='buea',field="software")
+print(my_profile )
+
+
+
+# 
+# def make_pizza(size,*toppings):
+#     print("making a "+ str(size)+ "-inch with the following toppings")
+#     for topping in toppings:
+#       print("- " +topping)
+
+# make_pizza(12,'mushrooms','carrot','beans')
+
+
+
+
+
+# def greet_user(username):
+#     print("Welcome "+ username.title()+ " to Joe ")
+
+# greet_user("joseph")
+# name="joseph is coming"
+# print(name.split(" "))
