f7f703419055a6d748eae71a46ff0ca63221e5c4
Fomonyuytar Joseph <josephneymar33@gmail.com>
2021-10-17 00:14:42 -0700

    init

--- car.py
+++ car.py
@@ -1,27 +1,68 @@
 class Car():
-    def __init__(self,make,model,year):
+    def __init__(self ,make,year,model):
         self.make=make
         self.model=model
         self.year=year
-        self.odometer_reading = 0
+        self.odometer_reading=0
+
     def get_descriptive_name(self):
         long_name=str(self.year)+' '+self.make+' '+self.model
         return long_name.title()
-    def read_odometer(self):
-        """Print a statement showing the car's mileage."""
-        print("This car has " + str(self.odometer_reading) + " miles on it.")
-    def update_odometer(self, mileage):
-       if mileage >= self.odometer_reading:
-           self.odometer_reading = mileage
-       else:
-           print("You can't roll back an odometer!")
-    def increment_odometer(self, miles):
-      self.odometer_reading += miles
 
-    
-my_new_car=Car('audi','a4',2016)
+    def read_odometer(self):
+        print('the car has'+ str(self.odometer_reading)+' miles on it')
+
+my_new_car=Car('audi','a4','2016')
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Car():
+#     def __init__(self,make,model,year):
+#         self.make=make
+#         self.model=model
+#         self.year=year
+#         self.odometer_reading = 0
+#     def get_descriptive_name(self):
+#         long_name=str(self.year)+' '+self.make+' '+self.model
+#         return long_name.title()
+#     def read_odometer(self):
+#         """Print a statement showing the car's mileage."""
+#         print("This car has " + str(self.odometer_reading) + " miles on it.")
+#     def update_odometer(self, mileage):
+#        if mileage >= self.odometer_reading:
+#            self.odometer_reading = mileage
+#        else:
+#            print("You can't roll back an odometer!")
+#     def increment_odometer(self, miles):
+#       self.odometer_reading += miles
+
+    
+# my_new_car=Car('audi','a4',2016)
+# print(my_new_car.get_descriptive_name())
+# my_new_car.read_odometer()
 
     
         
